b3a00a675c7978a4fde48990a7005eba5125b7ba
MRsajal <113850934+MRsajal@users.noreply.github.com>
2024-06-12 23:11:02 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function showMessage(event){
+function showMessage(event) {
     event.preventDefault();
     const email = event.target.email.value;
     console.log(email);
-    const message=document.getElementById("showText");
+    const message = document.getElementById("showText");
     console.log(message)
-    const printMessage=document.createElement("p");
-    printMessage.innerText=email;
+    const printMessage = document.createElement("p");
+    printMessage.innerText = email;
     message.appendChild(printMessage);
-    
+
 }
 const imageArray = [
     {
@@ -47,39 +47,53 @@ const imageArray = [
         price: "$11.00"
     }
 ];
+
+
 function foodItem(event) {
-    const imageContainer = document.getElementById("items");  
+    const imageContainer = document.getElementById("items");
     for (const item of imageArray) {
+        const svgNS = "http://www.w3.org/2000/svg";
+        const svg = document.createElementNS(svgNS, "svg");
+        svg.setAttribute("width", "20");
+        svg.setAttribute("height", "20");
+        svg.setAttribute("viewBox", "0 0 100 100");
+        const star = document.createElementNS(svgNS, "polygon");
+        star.setAttribute("fill", "#F48E28");
+        star.setAttribute("points", "50,5 61,35 95,35 67,57 78,91 50,70 22,91 33,57 5,35 39,35");
+        svg.appendChild(star);
         const projectImage = document.createElement("div");
         projectImage.classList.add("item-card");
 
-        const img=document.createElement("img");
-        img.src=item.url;
+        const img = document.createElement("img");
+        img.src = item.url;
         projectImage.appendChild(img);
 
-        const itemHead=document.createElement("div");
+        const itemHead = document.createElement("div");
         itemHead.classList.add("item-info");
-        const itemDiv=document.createElement("div");
+        const itemDiv = document.createElement("div");
         itemDiv.classList.add("item-name");
-        const heading=document.createElement("h3");
-        heading.innerText=item.name;
+        const heading = document.createElement("h3");
+        heading.innerText = item.name;
         itemDiv.appendChild(heading);
-        const price=document.createElement("p");
-        
-        price.innerText=item.rating;
-        itemDiv.appendChild(price);
+        const svgDiv = document.createElement("div");
+        svgDiv.classList.add("svgDiv");
+        svgDiv.appendChild(svg);
+        const price = document.createElement("p");
+        price.innerText = item.rating;
+        svgDiv.appendChild(price)
+        itemDiv.appendChild(svgDiv);
 
-        const itemPrice=document.createElement("div");
+        const itemPrice = document.createElement("div");
         itemPrice.classList.add("item_price");
-        const addCart=document.createElement("div");
+        const addCart = document.createElement("div");
         addCart.classList.add("add_to_cart");
-        const Cart=document.createElement("p");
-        Cart.innerText='Add to Cart';
+        const Cart = document.createElement("p");
+        Cart.innerText = 'Add to Cart';
         addCart.appendChild(Cart);
         itemPrice.appendChild(addCart);
-        const prices=document.createElement("p");
+        const prices = document.createElement("p");
         prices.classList.add("prices");
-        prices.innerText=item.price;
+        prices.innerText = item.price;
         itemPrice.appendChild(prices);
 
         itemHead.appendChild(itemDiv);
